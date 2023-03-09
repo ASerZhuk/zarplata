f9c4c116de77877319e632c9103761f7c2f69010
ASerZhuk <117305203+ASerZhuk@users.noreply.github.com>
2023-03-09 12:42:18 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 const vremy = prompt('Введите отработанное время: ')
 const procent = 8.20*60*1.15
 const zarplata = prompt('Введите зарплату: ')
-const summa = zarplata/procent
-const raznica = vremy - summa
+const summa = (zarplata/procent).toFixed(1)
+const raznica = (vremy - summa).toFixed(1)
 
 document.getElementById('vremy').innerHTML = vremy
 document.getElementById('zarplata').innerHTML = zarplata
