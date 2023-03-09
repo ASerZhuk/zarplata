3959e6a761cb1a3e8ed10fb1e1e917b36620077c
ASerZhuk <117305203+ASerZhuk@users.noreply.github.com>
2023-03-09 12:27:16 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,10 @@
-const vremy = prompt('Введите время: ') * 60
-const procent = (vremy * 15) / 100
-const vremy15 = vremy + procent
-const summa = Math.round(vremy15 * 8.2)
-const tarif = 490
+const vremy = prompt('Введите отработанное время: ')
+const procent = 8.20*60*1.15
 const zarplata = prompt('Введите зарплату: ')
-const raznica = summa - zarplata
+const summa = procent/zarplata
+const raznica = vremy - summa
 
-document.getElementById('vremy').innerHTML = vremy / 60
+document.getElementById('vremy').innerHTML = vremy
 document.getElementById('zarplata').innerHTML = zarplata
 document.getElementById('summa').innerHTML = summa
 document.getElementById('raznica').innerHTML = raznica
-document.getElementById('trf').innerHTML = (raznica / tarif).toFixed(3)
